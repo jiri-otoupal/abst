6c90de603751dacfafa0e532e973b8307028c679
jiri_otoupal <Lkw2Rn89p7yNJUMRD4fv>
2022-11-15 01:10:28 +0100

    Version numbering

--- abst/__version__.py
+++ abst/__version__.py
@@ -10,7 +10,7 @@ __description__ = (
     "CLI Command making OCI Bastion simple and fast"
 )
 
-__version__ = "1.5.1"
+__version__ = "1.5.2a1"
 __author__ = "Jiri Otoupal"
 __author_email__ = "jiri-otoupal@ips-database.eu"
 __license__ = "MIT"
