22f0af3089370a541f66bb03a7e4ea15638c8296
jiri_otoupal <Lkw2Rn89p7yNJUMRD4fv>
2022-12-06 14:48:20 +0100

    Added command for secret copy to different namespace

--- abst/__version__.py
+++ abst/__version__.py
@@ -10,10 +10,10 @@ __description__ = (
     "CLI Command making OCI Bastion and kubernetes usage simple and fast"
 )
 
-__version__ = "1.6.14"
+__version__ = "1.6.15"
 __author__ = "Jiri Otoupal"
 __author_email__ = "jiri-otoupal@ips-database.eu"
 __license__ = "MIT"
 __url__ = "https://github.com/jiri-otoupal/abst"
 
-__version_name__ = "Pistachio Icecream"
+__version_name__ = "Purring Kitten"
--- abst/main.py
+++ abst/main.py
@@ -380,6 +380,32 @@ def ssh_pod(pod_name):
         .split(" "))
 
 
+@cli.group("cp")
+def cp():
+    pass
+
+
+@cp.command("secret")
+@click.argument("secret_name")
+@click.argument("source_namespace")
+@click.argument("target_namespace")
+def cp_secret(secret_name: str, target_namespace: str, source_namespace: str = "default"):
+    """
+    Copy Secret in current cluster from source namespace to target
+    @param secret_name: Secret Name
+    @param target_namespace: Target Namespace name
+    @param source_namespace: Source Namespace name
+    @return:
+    """
+    try:
+        rich.print("Trying Copy secret")
+        os.system(
+            f"kubectl get secret {secret_name} --namespace={source_namespace} -o yaml | sed 's/namespace: .*/namespace: {target_namespace}/' | kubectl apply -f -")
+    except FileNotFoundError:
+        rich.print("[red]kubectl not found on this machine[/red]")
+        return
+
+
 def main():
     logging.basicConfig(
         level=logging.DEBUG,
