1a45b19f8397a93d6b9ddb23295e900c45595f26
jiri_otoupal <Lkw2Rn89p7yNJUMRD4fv>
2023-03-04 00:32:19 +0100

    Keep alive flag

--- abst/__version__.py
+++ abst/__version__.py
@@ -10,7 +10,7 @@ __description__ = (
     "CLI Command making OCI Bastion and kubernetes usage simple and fast"
 )
 
-__version__ = "1.8.9"
+__version__ = "1.9.0"
 __author__ = "Jiri Otoupal"
 __author_email__ = "jiri-otoupal@ips-database.eu"
 __license__ = "MIT"
