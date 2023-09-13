56a6ad8c4b40cc17a60a32bb739bcc425baffcb2
Jiri Otoupal <jiri.otoupal@oracle.com>
2023-09-13 17:00:54 +0200

    context display

--- abst/main.py
+++ abst/main.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import os
 import re
@@ -49,11 +50,22 @@ def context():
     pass
 
 
-@context.command("list")
+@context.command("list", help="Will list all contexts in ~/.abst/context/ folder")
 def _list():
     rich.print("Contexts:")
     for file in Path(default_contexts_location).iterdir():
         rich.print(f"    {file.name.replace('.json', '')}")
+
+
+@context.command(help="Will display JSON format of context")
+@click.argument("name")
+def display(name):
+    if name in [file.name.replace(".json", "") for file in Path(default_contexts_location).iterdir()]:
+        rich.print("[bold]Context config contents:[/bold]\n")
+        with open(Path(default_contexts_location) / (name + ".json"), "r") as f:
+            rich.print_json(data=json.load(f))
+    else:
+        rich.print("[red]Context does not exists[/red]")
 
 
 @parallel.command("add", help="Add Bastion to stack")
